2f5c9a58296ae7d9def64181f66b5b89bf0cafee
Leon Akasaka <leon@wisdomsoft.jp>
2016-02-17 01:51:22 +0900

    Add: IUpdatable interface.

---
+++ Levolution.Core.Shared/Component/IUpdatable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Levolution.Core.Component
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IUpdatable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        void Update();
+    }
+}
